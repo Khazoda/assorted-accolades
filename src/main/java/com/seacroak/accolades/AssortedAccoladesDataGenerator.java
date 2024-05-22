4c788db4258265724e862fb03e19340f0a55bcb9
Khazoda <khazoda@gmail.com>
2024-05-22 16:31:19 +0100

    Initial commit

---
+++ src/main/java/com/seacroak/accolades/AssortedAccoladesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.seacroak.accolades;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AssortedAccoladesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
