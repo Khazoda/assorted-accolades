550b80d93d88a8fc09d1af1aa595040aa3b71384
Khazoda <khazoda@gmail.com>
2024-05-24 19:43:37 +0100

    client render code

---
+++ src/client/java/com/seacroak/assorted_accolades/AccoladesModClient.java
@@ -0,0 +1,51 @@
+package com.seacroak.assorted_accolades;
+
+import com.seacroak.assorted_accolades.block.BaseTrophy;
+import com.seacroak.assorted_accolades.registry.MainRegistry;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.util.ActionResult;
+
+import java.util.concurrent.*;
+
+public class AccoladesModClient implements ClientModInitializer {
+
+	ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+	private static ScheduledFuture<?> future = null;
+	public static boolean trophyInteracted = false;
+	public static String trophyInteractedIdentifier = null;
+
+	@Override
+	public void onInitializeClient() {
+		BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BASE_TROPHY, RenderLayer.getCutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BINGO_COMPLETIONIST_TROPHY_2023, RenderLayer.getCutoutMipped());
+
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			MinecraftClient client = MinecraftClient.getInstance();
+			assert client.world != null;
+			Block block = client.world.getBlockState(hitResult.getBlockPos()).getBlock();
+			if (!player.isSpectator() && block instanceof BaseTrophy) {
+				trophyInteracted = true;
+				trophyInteractedIdentifier = block.getTranslationKey();
+				if (future != null) {
+					future.cancel(false);
+				}
+				future = executor.schedule(() -> {
+					trophyInteracted = false;
+					trophyInteractedIdentifier = null;
+				}, 3, TimeUnit.SECONDS);
+			}
+			return ActionResult.PASS;
+		});
+
+		/* THIS NEEDS TO HAPPEN SOMEWHERE FOR GARBO COLLECTION BUT I DONT KNOW WHERE AARGH!!*/
+		// executor.shutdown()
+
+	}
+}
---
+++ src/main/java/com/seacroak/assorted_accolades/AccoladesMod.java
@@ -0,0 +1,28 @@
+package com.seacroak.assorted_accolades;
+
+import com.seacroak.assorted_accolades.block.BaseTrophy;
+import com.seacroak.assorted_accolades.registry.ItemGroupRegistry;
+import com.seacroak.assorted_accolades.registry.MainRegistry;
+import com.seacroak.assorted_accolades.util.GenericUtils;
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.ActionResult;
+
+import static com.seacroak.assorted_accolades.Constants.ACCOLADES_ID;
+import static com.seacroak.assorted_accolades.Constants.ACCOLADES_LOGGER;
+
+public class AccoladesMod implements ModInitializer {
+    public static final ItemGroup BRONZE_ITEMGROUP = ItemGroupRegistry.createItemGroup();
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.ITEM_GROUP, GenericUtils.ID(ACCOLADES_ID), BRONZE_ITEMGROUP);
+        MainRegistry.init();
+        ACCOLADES_LOGGER.info("[Accolades] Finished Loading!");
+    }
+}
